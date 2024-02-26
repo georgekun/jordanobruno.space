a4e46f564d3d8db56fcbe7723ea3b0fb05839a9e
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-02-26 23:12:16 +0300

    add mimetypes and chane url

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import mimetypes
 
 
+mimetypes.add_type("text/css", ".css", True)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
