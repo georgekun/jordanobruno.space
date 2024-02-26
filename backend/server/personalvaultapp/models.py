0a5a1815a01cba352ceb6e1192383035e8cd28d0
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-02-26 16:03:25 +0300

    заданы форматы файлов в моделях

--- backend/server/personalvaultapp/models.py
+++ backend/server/personalvaultapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from django.core.validators import FileExtensionValidator
 
 class Profile(models.Model):
     name = models.CharField(max_length=60)
     age = models.IntegerField()
-    avatar = models.FileField(upload_to="_my/profile/")
+    
+    avatar = models.FileField(
+            upload_to="_my/profile/",
+            validators=[FileExtensionValidator(allowed_extensions=['png', 'jpeg'])])
+    
     number_phone = models.CharField(max_length=30)  
     email = models.CharField(max_length=30)
     telegram_link = models.CharField(max_length=20)
@@ -14,8 +18,15 @@ class Profile(models.Model):
 
 
 class Resume(models.Model):
-    resume_html = models.FileField(upload_to="_my/resume/html/")
-    resume_pdf = models.FileField(upload_to="_my/resume/pdf/")
+
+    resume_html = models.FileField(
+            upload_to="_my/resume/html/",
+            validators=[FileExtensionValidator(allowed_extensions=['html'])])
+    
+    resume_pdf = models.FileField(
+            upload_to="_my/resume/pdf/", 
+            validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
+                    
     created_at = models.DateTimeField(auto_now_add=True)
         
     def __str__(self):
--- backend/server/projectsapp/models.py
+++ backend/server/projectsapp/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-
+from django.core.validators import FileExtensionValidator
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     type_project = models.CharField(max_length=50)
     stack = models.CharField(max_length=255)
-    full_description = models.FileField(upload_to="_projects/")    
-    main_image = models.FileField(upload_to="_project/images/main/")
+    
+    full_description = models.FileField(
+            upload_to="_projects/",
+            validators=[FileExtensionValidator(allowed_extensions=['html'])])
+        
+    
+    main_image = models.FileField(
+            upload_to="_project/images/main/", 
+            validators=[FileExtensionValidator(allowed_extensions=['png', 'jpeg'])])
     
     def __str__(self):
         return f"{self.id} ==> {self.title}"
@@ -14,7 +21,11 @@ class Project(models.Model):
 
 class ProjectImage(models.Model):
     name_image = models.CharField(max_length=10, null=True)
-    image = models.FileField(upload_to="_project/images/")
+    
+    image = models.FileField(
+            upload_to="_project/images/",
+            validators=[FileExtensionValidator(allowed_extensions=['png', 'jpeg'])])
+    
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
