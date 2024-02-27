d5014cb64bf7909f494f2584ca6b072eba963b0d
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-02-27 14:38:31 +0300

    ссылки на pdf резюме

--- backend/server/personalvaultapp/serializers.py
+++ backend/server/personalvaultapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.core.files.storage import default_storage
 
 from .models import Profile, Resume 
 
@@ -12,7 +13,8 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class ResumeSerializer(serializers.ModelSerializer):
     resume_html = serializers.SerializerMethodField()
-    
+    resume_pdf = serializers.SerializerMethodField()
+
     class Meta:
         model = Resume
         exclude = ['id']
@@ -24,5 +26,9 @@ class ResumeSerializer(serializers.ModelSerializer):
         with open(instance.resume_html.path, "rb") as file:
             return file.read()
 
+    def get_resume_pdf(self, instance):
+        if instance.resume_pdf:
+            return default_storage.url(instance.resume_pdf.name)
+        return None
 
 
